79f0ea046a42eb0e65964afee784a0ef43c7c087
Jodie Sauer <jodie.sauer@docc.techstarter.de>
2022-12-03 18:31:11 +0100

    v2 massive overhaul

--- code.js
+++ code.js
@@ -1,117 +1,179 @@
+/////////////////////////////////////////////////////////////////////
+///////////////////// Globale Variablen ////////////////////////////
+// Variablen für compareInput() - Zähler für die Anzahl der Versuche, 
+// Array für bereits geratene Zahlen und die Zufallszahl.
+let numOfGuesses;
+let guessedNumbers;
+let randomNum;
+/////////////////////////////////////////////////////////////////////
+/////////////////////// Event Listener /////////////////////////////
+// Event Listener für Button "Start". Macht einen Callback auf startGame().
+function startGameButtonEventListener(){
+    let start = document.getElementById("start");
+    start.addEventListener("click", startGame);
+    return start;
+}
+startGameButtonEventListener();
+// Entfernt EL für Button "Start" (damit der Knopf nicht mehrfach gedrückt werden kann)
+function removeStartGameButtonEventListener(){
+    start.removeEventListener("click", startGame);
+}
+// Event Listener für "Neu starten"-Button
+function restartGameButtonEventListener(){
+    let restart = document.getElementById("restart");
+    restart.addEventListener("click", restartGame);
+}
+//////////////////////////////////////////////////////////////////////
+/////////////////////// Hilfs-Funktionen /////////////////////////////
 // Erstellen einer Zufallszahl
-let randomNo = Math.floor( Math.random() * 100 );
-    console.log(randomNo);
-
-// >> compareInput() - Zähler für die Anzahl der Versuche und array für bereits geratene Zahlen.
-let noOfGuesses = 0;
-let guessedNumbers = [];
-
+function randomNumGen(){
+    let randomNum = Math.floor( Math.random() * 100 );
+        console.log(randomNum);
+        /* loesung = document.createTextNode("Lösung " + randomNum);
+        document.body.appendChild(loesung); */
+        return randomNum;
+    }
+// Funktion zum Zurücksetzen der globalen Variablen
+// Wird zum Neustart des Spiels benötigt
+function resetGlobalVariables(){
+    randomNum = randomNumGen();
+    numOfGuesses = 0
+    guessedNumbers = [];
+}
+// Initiales Zurücksetzen der globalen Variablen
+resetGlobalVariables();
+// Ersetzt, falls vorhanden, den "Start"-Knopf durch einen "Neu starten"-Knopf.
+// Setzt außerdem die globalen Variablen zurück. Wird bei Erraten der richtigen Zahl ausgeführt.
+function replaceStartButton(){
+    if (document.getElementById("restart")){
+        resetGlobalVariables();
+    }
+    else {
+        let restart = document.createElement("button");
+        restart.textContent = "Neu starten";
+        restart.id = "restart";
+        let startChild = document.getElementById("start");
+        let startParent = startChild.parentNode;
+        startParent.replaceChild(restart, start);
+        restartGameButtonEventListener();
+        resetGlobalVariables();
+    }
+}
+// Allgemeine createElement-Funktion. Nimmt den Elementtyp und ein Objekt mit Attributwerten entgegenen.
+// Ein Element mit entsprechendem Typ wird erstellt. Danach wird durch die Attribute des Objekts iteriert 
+// und diese werden dem Element hinzugefügt. Am Ende wird das fertige Element dem HTML Main-Container hinzugefügt.
+// Hilfreiche Quellen: https://stackoverflow.com/questions/43168284/javascript-createelement-function
+// https://www.w3schools.com/jsref/met_document_createelement.asp
+function createElement(type, attributes){
+    let element = document.createElement(type);
+    for (var key in attributes){
+        element[key] = attributes[key];
+    }
+    document.getElementById("container").appendChild(element);
+}
+/////////////////////////////////////////////////////////////////////
+/////////////////////// Kern-Funktionen /////////////////////////////
+// Funktion, die das Spielfeld aufbaut. Wird über den Start-Button aufgerufen.
 function startGame(){
-
-    // Variable für Zufallszahl wird initialisiert.
-    // !!FRAGE!! Dies hat nicht funktioniert, da diese Variable in der zweiten Funktion s.u. nicht bekannt ist. 
-    // Wie reicht man diese Variable am besten an andere Funktionen weiter ohne sie global zu stellen?
-    /* let randomNo = Math.floor( Math.random() * 100 );
-    console.log(randomNo); */
-      
-    // Neues Element erstellen. Info an User.
-    const node = document.createElement("h1");
-    const textnode = document.createTextNode("Eine Zahl zwischen 0 und 100 wurde zufällig ausgewählt.");
-    node.appendChild(textnode);
-
-    // Zweites Element erstellen, damit Text in neue Zeile kommt. Geht bestimmt auch einfacher in einem einzigen Element, ich wusste aber nicht wie.
-    const node2 = document.createElement("h1");
-    const textnode2 = document.createTextNode("Gib deinen Tipp im Kästchen ein und drücke 'OK'");
-    node2.appendChild(textnode2);
+    /////// Spielfeld aufbauen
+    // Neue Text-Elemente erstellen. Info an User.
+    createElement("h3", {"textContent":"Eine Zahl zwischen 0 und 100 wurde zufällig ausgewählt.", "id":"info1"});
+    createElement("h3", {"textContent":"Gib deinen Tipp im Kästchen ein und drücke 'OK'", "id":"info2"});
 
     // Erstelle Input Feld mit Typ Zahlen.
-    const inputField = document.createElement("input");
-    inputField.setAttribute("type", "number");
-    inputField.setAttribute("id", "guess");
+    createElement("input", {"type":"number", "id": "guess"});
 
-    //Erstelle OK-Button
-    const button = document.createElement("button");
-    button.setAttribute("onclick", "compareInput()");
-    const buttonTxt = document.createTextNode("OK");
-    button.appendChild(buttonTxt);
+    // Erstelle OK-Button
+    createElement("button", {"textContent":"OK", "id":"ok-btn"});
 
-    //Erstelle eine leere Dialog-Zeile. Diese wird später in der Funktion compareInput() mit Infos an die User gefüllt.
-    const node3 = document.createElement("p");
-    const textnode3 = document.createTextNode("");
-    node3.appendChild(textnode3);
-    //Weitere Dialog-Zeile "Bereits geratene Zahlen"
-    const node4 = document.createElement("p");
-    const textnode4 = document.createTextNode("Bereits geratene Zahlen: ");
-    node4.appendChild(textnode4);
-    //Weitere Dialog-Zeile "Anzahl der Versuche"
-    const node5 = document.createElement("p");
-    const textnode5 = document.createTextNode("Anzahl der Versuche: ");
-    node5.appendChild(textnode5);
+    // Erstelle drei Dialog-Zeilen
+    createElement("p", {"textContent":"", "id":"message1"});
+    createElement("p", {"textContent":"Bereits geratene Zahlen: ", "id":"message2"});
+    createElement("p", {"textContent":"Anzahl der Versuche: ", "id":"message3"});
+    
+    /////// Event Listener
+    // Event Listener auf Ok-Button mit Callback auf compareInput().
+    let compare = document.getElementById('ok-btn');
+    compare.addEventListener("click", compareInput);
 
-    // Elemente dem Dokument anfügen.
-    document.getElementById("infotext").appendChild(node);
-    document.getElementById("infotext").appendChild(node2);
-    document.getElementById("eingabe").appendChild(inputField);
-    document.getElementById("btn-ok").appendChild(button);
-    document.getElementById("message1").appendChild(node3);
-    document.getElementById("message2").appendChild(node4);
-    document.getElementById("message3").appendChild(node5);
-
-
+    // Event Listener, der beim Drücken von "Enter" einen click auf den OK-Button auslöst.
+    // Dies geht nur, wenn das Input Feld aktiv ist, d.h. wenn darin eine Eingabe passiert. 
+    let input = document.getElementById("guess");
+    input.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            document.getElementById("ok-btn").click();
+        }
+    });
+    // Dies war nötig, damit das Spiel bei einem Neustart in keinen Referenz-Fehler läuft
+    // Ein Element mit der ID start ist nach einem Neustart nicht mehr vorhanden.
+    if (document.getElementById("start")){
+        removeStartGameButtonEventListener()
+    }
+    else {
+        console.log("Nichts tun");
+    }
 }
 
-function compareInput(){
+// Beginnt das Spiel von Neuem. Alle Elemente werden entfernt
+// und neu hinzugefügt. Das geht bestimmt auch eleganter
+function restartGame(){
+/*     if (document.getElementById("info1")){
+        // Alte Elemente entfernen */
+        document.getElementById("info1").remove();
+        document.getElementById("info2").remove();
+        document.getElementById("guess").remove();
+        document.getElementById("ok-btn").remove();
+        document.getElementById("message1").remove();
+        document.getElementById("message2").remove();
+        document.getElementById("message3").remove();
+        startGame();
+/*     }
+    else {
+        startGame();
+    } */
+}
 
+// Vergleicht die User-Eingabe mit der Zufallszahl, gibt Dialoge aus und
+// leitet bei Spielende einen Reset der globalen Variablen ein.
+function compareInput(){
     // Eingegebene Zahl einlesen
     let inputValue = document.getElementById("guess").value;
     // Die drei Dialogzeilen werden in Variablen gespeichert
     let msg1 = document.getElementById("message1");
     let msg2 = document.getElementById("message2");
-    let msg3 = document.getElementById("message3");
-    // 
-    
+    let msg3 = document.getElementById("message3");    
 
         if (inputValue>100 || inputValue<0){
                 alert("Deine Eingabe ist ungültig. Bitte eine Zahl zwischen 0-100 eingeben.");
         }
         else if 
             (!inputValue){
-                msg1.textContent = "Bitte eine Zahl eingeben.";
+                msg1.innerHTML = "<h1>Bitte eine Zahl eingeben.</h1>";
         }
         else{
-            guessedNumbers.push(inputValue);
-            noOfGuesses+= 1;
+            guessedNumbers.push(" " + inputValue);
+            numOfGuesses++;
                     
-            if (inputValue<randomNo){
+            if (inputValue<randomNum){
                 msg1.textContent = "Dein Tipp ist zu niedrig. Gib eine höhere Zahl ein.";
-                msg2.textContent = "Bereits geratene Zahlen: " + guessedNumbers;
-                msg3.textContent = "Anzahl der Versuche: " + noOfGuesses;
+                msg2.textContent = "Bereits geratene Zahlen: [" + guessedNumbers + " ]";
+                msg3.textContent = "Anzahl der Versuche: " + numOfGuesses;
             }
-            else if (inputValue>randomNo){
+            else if (inputValue>randomNum){
                 msg1.textContent = "Dein Tipp ist zu hoch. Gib eine niedrigere Zahl ein."
-                msg2.textContent = "Bereits geratene Zahlen: " + guessedNumbers;
-                msg3.textContent = "Anzahl der Versuche: " + noOfGuesses;
+                msg2.textContent = "Bereits geratene Zahlen: [" + guessedNumbers + " ]";
+                msg3.textContent = "Anzahl der Versuche: " + numOfGuesses;
             }
-            else if (inputValue==randomNo){
-                msg1.textContent = "Dein Tipp ist richtig. Glückwunsch, du hast gewonnen!"
+            else if (inputValue==randomNum){
+                msg1.innerHTML = "<h3>Glückwunsch, du hast gewonnen! <br/>Klicke oben auf 'Neu starten' für eine neue Runde.</h3>"
                 msg2.textContent = "Die richtige Zahl war die " + inputValue + ".";
-                msg3.textContent = "Du hast insgesamt " + noOfGuesses + " Versuche benötigt.";
+                msg3.textContent = "Du hast insgesamt " + numOfGuesses + " Versuche benötigt.";
+                document.getElementById("ok-btn").removeEventListener("click", compareInput);
+                replaceStartButton();
             }
             else {
                 console.log("Dieser Fall war nicht vorgesehen")
             }
         }
-            
-          
-   // document.getElementById("eingabe").reset();
 }
-
-let inputField1 = document.getElementById("eingabe");
-inputField1.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        document.getElementById("btn-ok").click();
-    }});
-
-let start = document.querySelector('button');
-start.addEventListener("click", startGame);
